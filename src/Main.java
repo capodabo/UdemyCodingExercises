fd36a9c06d8e6797a1379abaf7f2b6e205ef4062
capodabo <71343016+capodabo@users.noreply.github.com>
2021-11-23 21:50:08 +0000

    Branch for the coding exercise 8 - area calculator

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(12.0));
+        System.out.println(AreaCalculator.area(4.0,5));
+        System.out.println(AreaCalculator.area(-12));
+        System.out.println(AreaCalculator.area(-4,5));
+        System.out.println(AreaCalculator.area(4,-5));
+    }
+}
