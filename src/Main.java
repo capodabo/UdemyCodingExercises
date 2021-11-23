68861450dd6ebea82031f2f826b7a18faaf3e83c
capodabo <71343016+capodabo@users.noreply.github.com>
2021-11-23 22:32:45 +0000

    Branch for the coding exercise 11 - playing cat

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
+    }
+}
