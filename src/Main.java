bc1b68033c503b43967ae0921fecea0cd25e1272
capodabo <71343016+capodabo@users.noreply.github.com>
2021-11-24 21:20:06 +0000

    Branch for the coding exercise 12 - number in word

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(5);
+        NumberInWord.printNumberInWord(-5);
+        NumberInWord.printNumberInWord(25);
+    }
+}
