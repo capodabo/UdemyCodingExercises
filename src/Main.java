9c622b561adfbd9298357497fb7da44583af7f7f
capodabo <71343016+capodabo@users.noreply.github.com>
2022-01-31 21:23:02 +0000

    Branch for the coding exercise 18 - shared digit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SharedDigit.hasSharedDigit(12, 23));
+        System.out.println(SharedDigit.hasSharedDigit(9, 99));
+        System.out.println(SharedDigit.hasSharedDigit(15, 55));
+    }
+}
---
+++ src/SharedDigit.java
@@ -0,0 +1,10 @@
+public class SharedDigit {
+    public static boolean hasSharedDigit (int a, int b) {
+        if ((a < 10) || (a > 99) || (b < 10) || (b > 99)) {
+            return false;
+        }
+        else {
+            return ((a % 10 == b % 10) || (a / 10 == b /10) || (a /10 == b % 10) || (a % 10 == b / 10));
+        }
+    }
+}
