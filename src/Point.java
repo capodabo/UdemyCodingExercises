152110995e8ae95a00fd4db6140f51279725bc77
capodabo <71343016+capodabo@users.noreply.github.com>
2022-03-29 20:30:02 +0100

    Branch for the coding exercise 32 - point

---
+++ src/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt((0 - this.x) * (0 - this.x) + (0 - this.y) * (0 - this.y));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point obj) {
+        return Math.sqrt((obj.x - this.x) * (obj.x - this.x) + (obj.y - this.y) * (obj.y - this.y));
+    }
+}
