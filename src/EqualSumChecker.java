4bd82a4d7d7858ad3d34c3988f3236383e507d72
capodabo <71343016+capodabo@users.noreply.github.com>
2022-04-26 17:39:43 +0100

    Branch for the coding exercise 6 - equal sum checker

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,5 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int firstInt, int secondInt, int thirdInt) {
+        return firstInt + secondInt == thirdInt;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        EqualSumChecker.hasEqualSum(1,2,4);
+    }
+}
